ba3dee00611c8c58dcc2defde77cabbe37376e95
subarashii-huch09 <cwhu823@icloud.com>
2021-10-09 16:17:41 +1300

    first commit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -6,13 +6,52 @@ $(document).ready(function (e) {
     e.preventDefault();
     $(".trigger").toggleClass("active");
     $(".dropDown").slideToggle(500);
-  }); // scroll to top 效果
-
+  }); 
+  
+  
+  // scroll to top 效果
   $(".top").click(function (e) {
     e.preventDefault();
     $("html,body").animate({
       scrollTop: 0
     }, 1000);
   });
+
+
+  // swiper
+  const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  effect: 'fade',
+  fadeEffect: { crossFade: true},
+  speed: 2500,
+  spaceBetween: 150,
+  autoplay: {
+    delay: 5000,
+  },
+
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+lightbox.option({
+  resizeDuration: 700,
+  wrapAround: true,
+  fadeDuration: 700,
+  positionFromTop: 300,
+});
+
+
+
 });
 //# sourceMappingURL=all.js.map
